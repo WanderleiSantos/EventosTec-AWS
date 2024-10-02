f7c1ad4deddba9e27e5bc7125c7a8188b858ac64
Wanderlei Santos <wanderleisantosdias@gmail.com>
2024-10-01 21:42:50 -0300

    feat: Creating Entities

---
+++ src/main/java/com/manodev/eventostec/api/domain/coupon/Coupon.java
@@ -0,0 +1,33 @@
+package com.manodev.eventostec.api.domain.coupon;
+
+import com.manodev.eventostec.api.domain.event.Event;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+import java.util.UUID;
+
+@Table(name = "coupon")
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Coupon {
+
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    private String code;
+    private String discount;
+    private Date valid;
+
+    @ManyToOne
+    @JoinColumn(name = "event_id")
+    private Event event;
+
+}
