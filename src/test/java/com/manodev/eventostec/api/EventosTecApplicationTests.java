cadbb4e6a86464bdbbe136f7d270edba77a0cce8
Wanderlei Santos <wanderleisantosdias@gmail.com>
2024-10-01 21:42:23 -0300

    First Commit

---
+++ src/test/java/com/manodev/eventostec/api/EventosTecApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manodev.eventostec.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventosTecApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
